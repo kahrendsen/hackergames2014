e14a5c710b149b427911ab102bc9adb20a7750d9
Winston Wu <wswu24@gmail.com>
2014-07-12 01:58:21 -0500

    added sample places.sqlite and program for reading it

---
+++ src/readhistory.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+placesPath = '../places.sqlite'
+
+def main():
+	conn = sqlite3.connect(placesPath)
+	
+	c = conn.cursor()
+	
+	c.execute('select sqlite_version()')
+	print c.fetchall()
+	
+	c.execute("""select * from moz_historyvisits""")
+	
+	history = c.fetchall()
+	print history
+	
+	conn.close()
+	
+if __name__ == '__main__':
+	main()
